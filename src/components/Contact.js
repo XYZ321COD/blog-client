d31030fe618b32411f3464397c525471df0bbaec
XYZ321COD <michalznalezniak2@gmail.com>
2020-06-22 20:04:50 +0200

    Change Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
-import './Home.css'
+import './Contact.css'
 import Aggregator from "./ReUsableComponents/Aggregator";
 import CatAndPost from "./ReUsableComponents/ContainerForCategoriesAndRecentPosts";
 
@@ -11,9 +11,9 @@ export default class Contact extends Component {
         return(
             <Col>
                 <div className="jumbotron jumbotron_custom">
-                    <h1> Hello :) </h1>
-                    <h2> If you have any questions, you can contact me via:</h2>
-                    <div> Mail: michalznalezniak@gmail.com</div>
+                    <h2 > Hello :) </h2>
+                    <div className="text_welcome"> If you have any questions about my posts or cool projects offers that belong to my interests you can contact me via:</div>
+                    <div className="text_welcome"> Mail: michalznalezniak@gmail.com</div>
                     <Link href="https://www.linkedin.com/in/micha%C5%82-znale%C5%BAniak-b17b81182/"> LinkedIn </Link>
                 </div>
             </Col>
